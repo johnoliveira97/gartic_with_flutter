4939eb2b21404ebcd7db8c0041c0dbd4d9b29e47
João Pedro Pires de Oliveira <john_oliveira97@hotmail.com>
2023-10-22 15:48:42 -0300

    Alterando arquivos
    Incluindo funções para desenhar.

---
+++ lib/model/message.dart
@@ -0,0 +1,6 @@
+class Message {
+  final String user;
+  final String x;
+
+  Message(this.user, this.x);
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gartic_with_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GarticApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
